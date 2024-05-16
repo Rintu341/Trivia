cba3189b20e4501740d9246f01978dbd0f788ec5
sujan <sujanrintu@gmail.com>
2024-05-16 18:19:20 +0530

    Radio button ui create

--- app/src/main/java/com/example/jettriviaapp/component/Question.kt
+++ app/src/main/java/com/example/jettriviaapp/component/Question.kt
@@ -2,18 +2,31 @@ package com.example.jettriviaapp.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -21,9 +34,9 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jettriviaapp.model.QuestionItem
 import com.example.jettriviaapp.screen.QuestionViewModel
 import com.example.jettriviaapp.util.AppColors
 
@@ -35,18 +48,44 @@ fun Questions(viewModel: QuestionViewModel) {
         Log.d("LOADING","QUESTIONS IS LOADING")
 
     }else{
-        Log.d("LOADING","QUESTIONS LOADING IS STOPPED")
-        questions?.forEach {
-            Log.d("Result", " Question = ${it.question}")
+        if(questions != null )
+        {
+            QuestionDisPlay( question = questions.first())
         }
 
     }
     Log.d("SIZE","Questions = ${questions?.size}")
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun QuestionDisPlay(modifier: Modifier = Modifier) {
+fun QuestionDisPlay(
+    question: QuestionItem,
+//    questionIndex : MutableState<Int>,
+//    viewModel: QuestionViewModel,
+    onNextClicked:(Int) -> Unit = {}
+    ) {
+
+    //store list of answer
+    val choiceState = remember(question) {
+        question.choices.toMutableList()
+    }
+
+    // store the index of the answer that user taped
+    val answerState = remember(question){
+            mutableStateOf<Int?>(null)
+    }
+
+     val correctAnswerState = remember(question) {
+         mutableStateOf<Boolean?>(null)
+     }
+
+    val updateAnswer:(Int) -> Unit = remember(question) {
+        {
+            answerState.value = it
+            correctAnswerState.value =  choiceState[it] == question.answer
+        }
+    }
 
     val pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(10f,10f),5f)
     Surface(
@@ -62,6 +101,55 @@ fun QuestionDisPlay(modifier: Modifier = Modifier) {
         ) {
             QuestionTracker()
             DrawDottedLine(pathEffect = pathEffect)
+
+            Text(text = question.question,
+                modifier = Modifier
+                    .padding(6.dp)
+                    .align(alignment = Alignment.Start)
+                    .fillMaxHeight(0.3f)
+                    .fillMaxWidth(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = AppColors.mOffWhite,
+                lineHeight = 22.sp)
+            //choices
+            choiceState.forEachIndexed { index, answerText ->
+                Row(
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .fillMaxWidth()
+                        .height(45.dp)
+                        .border(
+                            width = 4.dp, brush = Brush.linearGradient(
+                                colors = listOf(
+                                    AppColors.mDarkPurple,
+                                    AppColors.mOffDarkPurple
+                                )
+                            ),
+                            shape = RoundedCornerShape(15.dp)
+                        )
+                        .clip(shape = RoundedCornerShape(50))
+                        .background(Color.Transparent),
+                    verticalAlignment = Alignment.CenterVertically
+                )
+                {
+                        RadioButton(selected = (answerState.value == index) ,
+                            onClick = {
+                                updateAnswer(index)
+                            },
+                            modifier = Modifier.padding(start = 16.dp),
+                            colors = RadioButtonDefaults.colors(
+                                selectedColor = (if(correctAnswerState.value == true && index == answerState.value){
+                                    Color.Green.copy(0.2f)
+                                }else{
+                                    Color.Red.copy(0.2f)
+                                })
+                            )) // End radio
+
+                    Text(text = answerText)
+                }
+            }
+
         }
     }
 }
